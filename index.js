689baf099ec038eaa064a00594ffad3a78e3e9db
your name <your@email.com>
2022-02-04 22:18:22 -0300

    Primer commit

---
+++ index.js
@@ -0,0 +1,84 @@
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+
+// const { Server } = require('http');
+
+
+// const { clearScreenDown } = require('readline');
+
+// const textIn = fs.readFileSync('./input.txt', 'utf-8'); // Funcion que lee un txt
+
+// const textOut = `Esta es una escritura en el archivo: ${textIn}`;
+// fs.writeFileSync('./ouput.txt', textOut); // Funcion que crea y escribe un archivo
+
+// const hello = 'hola mundo';
+// console.log(hello);
+// console.log(textIn);
+// console.log(textOut);
+// console.log('Archivo creado y escrito!');
+
+
+/// WEB BASIC SERVER
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+
+
+const replaceTemplate = (temp, product) => {
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output.replace(/{%IMAGE%}/g, product.image);
+    output.replace(/{%FROM%}/g, product.from);
+    output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output.replace(/{%QUANTITY%}/g, product.quantity);
+    output.replace(/{%PRICE%}/g, product.price);
+    output.replace(/{%ID%}/g, product.id);
+    output.replace(/{%DESCRIPTION%}/g, product.description);
+
+
+
+    if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+    return output;
+
+}
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+
+const dataObj = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+
+    const pathName = req.url;
+
+    if (pathName === '/' || pathName === '/overview') {
+
+        res.writeHead(200, { 'Content-type': 'text/html' });
+
+        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+        const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+
+        console.log(cardsHtml);
+
+        res.end(output);
+
+
+    } else if (pathName === '/producto') {
+        res.end('Producto');
+
+
+    } else if (pathName === '/api') {
+
+        res.writeHead(200, { 'Content-type': 'application/json' }); // Para que se vean los iconos de los objetos del json
+        res.end(data);
+
+    } else {
+        res.end('<h1>Pagina no encontrada!</h1>');
+    }
+
+
+});
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Escuchando el servidor en el puerto 8000');
+});
