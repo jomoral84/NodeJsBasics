2c7eeaa7c2404c92c04d6d4134955f9ed822c017
Jor <you@example.com>
2022-02-07 10:42:16 -0300

    Soluciono problema de los iconos

--- index.js
+++ index.js
@@ -26,21 +26,20 @@ const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'u
 const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
 
 
+
+
 const replaceTemplate = (temp, product) => {
     let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-    output.replace(/{%IMAGE%}/g, product.image);
-    output.replace(/{%FROM%}/g, product.from);
-    output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-    output.replace(/{%QUANTITY%}/g, product.quantity);
-    output.replace(/{%PRICE%}/g, product.price);
-    output.replace(/{%ID%}/g, product.id);
-    output.replace(/{%DESCRIPTION%}/g, product.description);
-
-
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%ID%}/g, product.id);
 
     if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
     return output;
-
 }
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
